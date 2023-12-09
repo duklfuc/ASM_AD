a832356e757e2a32e1396a1115305d8550fe8ece
duklfuc <tranducphuc20072003@gmail.com>
2023-12-09 09:30:10 +0700

    first commit

---
+++ Tranning/DatabaseConnection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Tranning
+{
+    public class DatabaseConnection
+    {
+        public static string GetStrConnection()
+        {
+            string strConnection = @"Data Source=DUK-FUC;Initial Catalog=Tranning;User ID=sa;Password=20072003;TrustServerCertificate=True;";
+            return strConnection;
+        }
+
+        public static SqlConnection GetSqlConnection()
+        {
+            string strConnection = DatabaseConnection.GetStrConnection();
+            SqlConnection connection = new SqlConnection(strConnection);
+            return connection;
+        }
+    }
+}
